f3254c3645e195e44d1c5a4a782477e331c9c96d
Edgar Maldonado <ed.dev28@gmail.com>
2024-06-14 23:58:12 -0500

    fix metadatos en el header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content={metadata.description ?? ""} />
         <title>{String(metadata.title ?? "")}</title>
       </Head>
