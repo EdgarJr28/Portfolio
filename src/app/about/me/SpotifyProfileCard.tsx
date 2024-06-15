dc6d6be52e2aec40c0d034ba243ed7dde1e25f43
Edgar Maldonado <ed.dev28@gmail.com>
2024-06-15 00:59:01 -0500

    fix spotify responsive

--- src/app/about/me/SpotifyProfileCard.tsx
+++ src/app/about/me/SpotifyProfileCard.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 
 const SpotifyProfileCard = () => {
     return (
-        <div className="max-w-96 mx-auto bg-green-200 dark:bg-transparent backdrop-opacity-40 my-10 rounded-lg overflow-hidden">
-            <div className="p-2 mdsm:max-w-full  mdsm:mx-[6%] mx-auto bg-green-200  dark:bg-transparent backdrop-opacity-40">
-                <iframe
-                    className="w-full drop-shadow-2xl"
-                    src="https://spotify-github-profile.vercel.app/api/view?uid=12179838533&cover_image=true&theme=default&show_offline=false&background_color=transparent&interchange=true&bar_color=transparent&bar_color_cover=true"
-                    width="300"
-                    height="380"
-                    frameBorder="0"
-                    allow="encrypted-media"
-                ></iframe>
-            </div>
+
+        <div className="flex justify-center items-center w-full h-full">
+            <iframe
+                className="drop-shadow-2xl mr-2"
+                src="https://spotify-github-profile.vercel.app/api/view?uid=12179838533&cover_image=true&theme=default&show_offline=false&background_color=transparent&interchange=true&bar_color=transparent&bar_color_cover=true"
+                width="300"
+                height="380"
+                frameBorder="0"
+                allow="encrypted-media"
+            ></iframe>
+
+
         </div>
     )
 }
