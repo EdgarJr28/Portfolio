65db1fbecf569f81b29b0f9db821d3d37b24d7c7
Edgar Maldonado <ed.dev28@gmail.com>
2024-07-04 16:00:17 -0500

    fixed styles in playlist card

--- src/app/about/me/PlayListMusicModal.tsx
+++ src/app/about/me/PlayListMusicModal.tsx
@@ -18,8 +18,8 @@ const PlayListMusicModal = ({ isOpen, onClose }: any) => {
         if (isOpen) {
             fetchGetPlayList();
         }
-    }, [])
-    
+    }, [isOpen])
+
     if (!isOpen) return null;
 
 
@@ -27,15 +27,15 @@ const PlayListMusicModal = ({ isOpen, onClose }: any) => {
         <ModalContainer onClose={onClose}>
             <div className="bg-white dark:bg-dark-100 rounded-lg p-2 max-w-md w-full transition-all duration-300">
                 {Playlists ? <p className="text-center text-green-600 font-bold" >{Playlists[0].owner.display_name}</p> : ''}
-                <h3 className='text-green-600 text-center p-2'>
+                <h3 className={`text-green-600 text-center p-2 ${Playlists ? '' : 'mt-4 mx-8'} `}>
                     <FontAwesomeIcon size='1x' icon={faSpotify} color='#1DB954' />
                     &nbsp;
-                    My Playlist on Spotify
+                    My Playlists on Spotify
 
                     {Playlists ?
                         (<PlayListCarousel data={Playlists} />) :
                         <>
-                            <p className='text-center text-xs p-8'>Loading...</p>
+                            <p className='text-center text-xs p-8 '>Loading...</p>
                         </>
                     }
                 </h3>
