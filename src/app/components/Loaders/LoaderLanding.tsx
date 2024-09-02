753af6bee2308119a2794d947ccc36066c6753b7
Edgar Maldonado <ed.dev28@gmail.com>
2024-09-01 23:01:46 -0500

    chore: Update LoaderLanding component with pulsating animation and adjust icon size

--- src/app/components/Loaders/LoaderLanding.tsx
+++ src/app/components/Loaders/LoaderLanding.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faConnectdevelop } from '@fortawesome/free-brands-svg-icons';
 
 const LoaderLanding = () => {
     return (
-        <div className="flex flex-col justify-center items-center min-h-screen dark:text-white">
-            <FontAwesomeIcon
-                icon={faConnectdevelop}
-                width={90}
-                height={90}
-                size='5x'
-                className="animate-spin transition-all duration-300 mb-4 box-shadow-lg"
-            />
-            <h2 className="text-xl font-semibold">EdDev</h2>
+        <div className="flex flex-col justify-center items-center min-h-screen dark:text-white spin space-y-2">
+            <div className="relative pulsating">
+                <FontAwesomeIcon
+                    icon={faConnectdevelop}
+                    width={64}
+                    height={64}
+                    size="4x"
+                    className="pulsating"
+                />
+            </div>
+            <h2 className="text-xl font-semibold pulsatingText">EdDev</h2>
         </div>
-    )
-}
+    );
+};
 
-export default LoaderLanding
+export default LoaderLanding;
